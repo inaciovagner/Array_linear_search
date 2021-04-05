b68269c4233b9cf03df10d4fdb6fcf2154ce2546
Vagner de Souza <inaciovagner1@gmail.com>
2021-04-05 11:45:05 +0100

    Add project files.

---
+++ Array_linear_search/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_linear_search
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 4;
+            int location, i;
+            int[] a = {2,3,4,5,6,7};
+            for (i = 0; i < a.Length; i++)
+            {
+                if (a[i] == num)
+                {
+                    location = i;
+                    {
+                        Console.WriteLine(location);
+                    }
+                }
+            }
+        }
+    }
+}
